dfaa65a42416816c0c64f0810b44629514161f95
JerryTan <jryz101@gmail.com>
2019-08-05 04:15:53 +0800

    Initial Commit

---
+++ Core Data Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Core Data Demo
+//
+//  Created by Jerry Tan on 05/08/2019.
+//  Copyright © 2019 Starknet Technologies®. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
