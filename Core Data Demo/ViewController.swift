7b950ad0264cf8f37e9818b001a98309809d1a87
JerryTan <jryz101@gmail.com>
2019-08-05 04:47:17 +0800

    2. Import Core Data framework,  casting the delegate of the app to AppDelegate, creates context to tap into Core Data persistent container, Creates, configures, and returns an instance of the class for the entity with a given name

--- Core Data Demo/ViewController.swift
+++ Core Data Demo/ViewController.swift
@@ -1,20 +1,28 @@
-//
 //  ViewController.swift
 //  Core Data Demo
-//
 //  Created by Jerry Tan on 05/08/2019.
 //  Copyright © 2019 Starknet Technologies®. All rights reserved.
-//
+
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+        
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Casting the delegate of the app object to AppDelegate
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        //The managed object context associated with the main queue. (read-only) | Access core database methods
+        let context = appDelegate.persistentContainer.viewContext
+        
+        //Creates, configures, and returns an instance of the class for the entity with a given name
+        let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
     }
-
-
 }
 
