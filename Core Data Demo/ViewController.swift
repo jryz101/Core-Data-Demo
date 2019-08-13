3517ff53908981e962b37fcedaa389afa48375d1
JerryTan <jryz101@gmail.com>
2019-08-13 08:16:28 +0800

    4. Construct NSFetchRequestResult and do try catch construction block, casting data in to String, fetching saved data from core data model and log error.

--- Core Data Demo/ViewController.swift
+++ Core Data Demo/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
         
         
         //Sets the property of the receiver specified by a given key to a given value.
-        newUser.setValue("Leonado", forKey: "username")
+        newUser.setValue("Captain", forKey: "username")
         newUser.setValue("myPass", forKey: "password")
         newUser.setValue(35, forKey: "age")
         
@@ -42,6 +42,43 @@ class ViewController: UIViewController {
             print("There was an error")
         }
         
+        
+        //A description of search criteria used to retrieve data from a persistent store | Retrieve data from core data model
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        //A Boolean value that indicates whether the objects resulting from a fetch request are faults
+        request.returnsObjectsAsFaults = false
+        
+        
+        //Do try catch construction block for fetching data from core data
+        do{
+            
+            let results = try context.fetch(request)
+            
+            //Conditional methods to check the results value whether is greater than 0
+            if results.count > 0 {
+                
+                //For in loop construction for casting the results into A generic class that implements all the basic behavior required of a Core Data model object
+                for result in results as! [NSManagedObject] {
+                    
+                    //Retrieve username value from core data and casting into String value
+                    if let username = result.value(forKey : "username") as? String {
+                        
+                        print(username)
+                        
+                    }
+                    
+                }
+                
+            }else{
+                
+                print("No Result")
+            }
+            
+        }catch{
+            
+                print("Couldn't Fetch Results")
+        }
     }
 }
 
